1cd69cf7579dd9add4ea1f243bb971496eeac303
Avanade <Avanade@WIN-FRCWARYBEU.corp.avanade.org>
2018-11-05 21:46:34 -0200

    adicionando gh-pages

--- aula5/src/app/store/reducers/auth.reducer.ts
+++ aula5/src/app/store/reducers/auth.reducer.ts
@@ -2,34 +2,34 @@ import { ActionReducer } from '@ngrx/store';
 import { AuthService } from 'src/app/auth.service';
 
 export interface AuthState {
-  user: {
-    localId: string,
-    email: string,
-  };
-  token: string;
+ user: {
+   localId: string,
+   email: string,
+ };
+ token: string;
 }
 
 const initialState = {
-  user: null,
-  token: null,
+ user: null,
+ token: null,
 };
 
-export const authReducer: ActionReducer<AuthState> = (state = initialState, action: any) => {
+export function authReducer (state = initialState, action: any) {
 
-  switch (action.type) {
-    case 'SET_USER':
-    state = {
-      ...state,
-      ...action.payload,
-    };
-    break;
-    case 'LOGOUT':
-    state = {
-      user: null,
-      token: null,
-    };
-    break;
-  }
+ switch (action.type) {
+   case 'SET_USER':
+   state = {
+     ...state,
+     ...action.payload,
+   };
+   break;
+   case 'LOGOUT':
+   state = {
+     user: null,
+     token: null,
+   };
+   break;
+ }
 
-  return state;
-};
+ return state;
+}
